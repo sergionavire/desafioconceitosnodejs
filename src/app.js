4a00c696d78546ec6c6d0520993345563b38a740
sergionavire <56460132+sergionavire@users.noreply.github.com>
2020-05-07 05:54:05 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  const query = request.query;
+  
+  return response.json(query);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  
+  repositories.push(repository);
+
+  return response.json(repository);
+
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: 'Repository not find' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json();
+  };
+
+  repositories.splice(repositoryIndex,1);
+
+  return response.status(240).send();
+
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
+
+module.exports = app;
